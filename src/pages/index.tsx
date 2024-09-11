84ff8049319cf9acb7b378431aea750bebd4bec7
Aaron <aaron@forloopconsulting.com>
2024-09-11 14:11:46 +0100

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import * as tus from "tus-js-client";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,6 +19,8 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div>Is supported: {tus.isSupported.toString()}</div>
+
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
